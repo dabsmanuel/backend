8db07597a01a851e2b7d5143e0dfad74e5c7b0ea
dabsmanuel <dabsmanuel5015@gmail.com>
2024-11-19 17:43:07 +0100

    resolved cors error

--- app.js
+++ app.js
@@ -15,10 +15,9 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: ['http://localhost:3000', 'https://koinfest.org', 'http://api.koinfest.org'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
