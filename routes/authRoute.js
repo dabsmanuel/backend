9af80237e77312496459d10920c2a9eb67a4a320
dabsmanuel <dabsmanuel5015@gmail.com>
2024-11-22 00:56:23 +0100

    created a forgottenPasword logic

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword/:email', authController.resetPassword);
+router.patch('/resetPassword/:email', authController.resetPassword);
 
 
 // Routes for super admin
