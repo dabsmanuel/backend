855c6ad9f4dd9a96a771c10e147a298c731b41e3
dabsmanuel <dabsmanuel5015@gmail.com>
2024-11-23 02:07:41 +0100

    removed password hashing

--- models/User.js
+++ models/User.js
@@ -73,11 +73,11 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 }, { timestamps: true });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
+// userSchema.pre('save', async function(next) {
+//   if (!this.isModified('password')) return next();
+//   // this.password = await bcrypt.hash(this.password, 12);
+//   next();
+// });
 
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
