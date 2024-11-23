6a7700a37d5515ed5302f6120fe370092218a3f0
dabsmanuel <dabsmanuel5015@gmail.com>
2024-11-23 01:41:59 +0100

    fixed resetPassword

--- models/User.js
+++ models/User.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
-    select: false,
+    select: true,
   },
   bitcoinWalletAddress: {
     type: String,
